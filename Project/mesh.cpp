ced75f4df4d64b866377ad03d393c5f31f68ac63
Handley-Michael <mhandley@brutha.(none)>
2015-01-29 13:15:01 +0000

    changed file names

---
+++ Project/mesh.cpp
@@ -0,0 +1,31 @@
+//Contains the function definitions for the Mesh class
+
+
+#include "S_mesh.h"
+
+//CONSTRUCTOR
+//Allocates memory to the 2D array of meshpoints
+Mesh::Mesh(int n_dimX, int n_dimY): mn_dimX(n_dimX),
+				    mn_dimY(n_dimY)
+{
+  //allocate memory for x array
+  matrix = new meshpoint*[mn_dimX];
+
+  //allocate memory for y arrays
+  for (int i=0; i<mn_dimX; i++){
+    matrix[i] = new meshpoint[mn_dimY];
+  }
+}
+
+//DESTRUCTOR
+//Deallocates memory from the 2D array of meshpoints
+Mesh::~Mesh()
+{
+  //deallocate y arrays
+  for (int i=0; i<mn_dimX; i++){
+    delete[] matrix[i];
+  }
+
+  //deallocate x array
+  delete[] matrix;
+}
