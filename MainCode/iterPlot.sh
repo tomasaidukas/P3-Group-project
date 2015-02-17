6eb779fa80cf6b670d409f4f4083c294d7cb1529
niga <tomasaiduk@gmail.com>
2015-02-17 14:08:48 +0000

    format for the output

--- MainCode/iterPlot.sh
+++ MainCode/iterPlot.sh
@@ -2,7 +2,15 @@
 
 set terminal png
 set output "Analyze/iterations.png"
-set title "Iterations vs Mesh size"
-set xlabel "X"
-set ylabel "Y"
+set title "Iterations vs Mesh size (square)"
+set xlabel "Mesh size (square dim)"
+set ylabel "Iteration number"
 plot "Analyze/VcompIterations.txt" with lines
+
+
+set terminal png
+set output "Analyze/time.png"
+set title "Time vs Mesh size (square)"
+set xlabel "Mesh size (square dim)"
+set ylabel "Time"
+plot "Analyze/VcompTime.txt" with lines
