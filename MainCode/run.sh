4003e07e4e9a4bf715c204376cdc318348548cdc
Handley-Michael <mhandley@brutha.(none)>
2015-02-19 13:17:51 +0000

    added cputime monitor

---
+++ MainCode/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear  #clear user screen
+>cputime.txt #empty cputime file
+EPOCH=`date +%s%N | cut -b1-13` #get time at start
+
+#NAME OF COMPILED PROCESS
+FILENAME='./main'
+
+VAR=`ps -u $USER r | grep $FILENAME | cut -d' ' -f1` #get process id
+while ps -u $USER r | grep $VAR > /dev/null #while process is running
+do
+CURTIME=`date +%s%N | cut -b1-13` #get current time
+ELAPSED=$(($CURTIME-$EPOCH)) #calculate time elapsed
+
+#output cpu usage
+echo "$ELAPSED "`ps -p $VAR --no-headers -o %cpu` >> cputime.txt
+done
