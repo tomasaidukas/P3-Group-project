49b53d8c0e91b8f36249f74475e1cefdb5e0d5b6
Handley-Michael <mhandley@brutha.(none)>
2015-02-26 14:09:33 +0000

    run.sh calculates cpu usage per core now

--- MainCode/run.sh
+++ MainCode/run.sh
@@ -7,12 +7,14 @@ EPOCH=`date +%s%N | cut -b1-13` #get time at start
 #NAME OF COMPILED PROCESS
 FILENAME="./main"
 
-VAR=`ps -u $USER r | grep $FILENAME | cut -d ' ' -f1` #get process id
+VAR=`ps -u $USER r | grep $FILENAME | cut -d ' ' -f2` #get process id
 while ps -u $USER r | grep $VAR > /dev/null #while process is running
 do
 CURTIME=`date +%s%N | cut -b1-13` #get current time
 ELAPSED=$(($CURTIME-$EPOCH)) #calculate time elapsed
+CPUUSAGE=`ps -p $VAR --no-headers -o %cpu`
+NUMCORES=`nproc | cut -d$'\n' -f1`
 
 #output cpu usage
-echo "$ELAPSED "`ps -p $VAR --no-headers -o %cpu` >> cputime.txt
+echo "$ELAPSED "`bc <<< "scale=2; $CPUUSAGE / $NUMCORES"` >> cputime.txt
 done
